0a4ecd949ad6df032b9824715b5a9d50324920ba
KillianRipoche <killian.ripoche@gmail.com>
2026-03-27 09:47:02 +0100

    push algo

---
+++ double_circular_linked_list/0-add_node.c
@@ -0,0 +1,75 @@
+#include <string.h>
+#include "list.h"
+
+/**
+ * add_node_end - Add a new node to the end of a double circular linked list
+ * @list: pointer to the head of the list
+ * @str: string to copy into the new node
+ *
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+	List *new_node;
+	List *tail;
+
+	new_node = malloc(sizeof(List));
+	if (!new_node)
+		return (NULL);
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	if (!*list)
+	{
+		new_node->prev = new_node;
+		new_node->next = new_node;
+		*list = new_node;
+		return (new_node);
+	}
+	tail = (*list)->prev;
+	new_node->prev = tail;
+	new_node->next = *list;
+	tail->next = new_node;
+	(*list)->prev = new_node;
+	return (new_node);
+}
+
+/**
+ * add_node_begin - Add a new node to the beginning of a double circular list
+ * @list: pointer to the head of the list
+ * @str: string to copy into the new node
+ *
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+	List *new_node;
+	List *tail;
+
+	new_node = malloc(sizeof(List));
+	if (!new_node)
+		return (NULL);
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	if (!*list)
+	{
+		new_node->prev = new_node;
+		new_node->next = new_node;
+		*list = new_node;
+		return (new_node);
+	}
+	tail = (*list)->prev;
+	new_node->prev = tail;
+	new_node->next = *list;
+	tail->next = new_node;
+	(*list)->prev = new_node;
+	*list = new_node;
+	return (new_node);
+}
